2eca05538f8f884111835ad2a4a792781dddd2b5
Vinicius Gomes <vinigomescunha@gmail.com>
2020-03-10 13:06:09 -0300

    livros router

--- routes/livros.js
+++ routes/livros.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 // eu injeto o modelo nas rotas 
 module.exports = async (model) => {
-  router.use(function timeLog(req, res, next) {
+  
+  router.use((req, res, next) => {
     console.log('Time: ', Date.now());
     next();
   });
@@ -13,7 +14,9 @@ module.exports = async (model) => {
   });
 
   router.get('/:id', async (req, res) => {
-    res.send(await model.get(parseInt(req.params.id)));
+    const lv = await model.get(parseInt(req.params.id));
+    if(!lv) res.status(404)
+    res.send(lv[0]);
   });
 
   return router;
